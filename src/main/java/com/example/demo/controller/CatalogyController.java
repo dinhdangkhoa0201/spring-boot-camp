96dc9d5c1ec22b44afa741024655a2ba7d99fde9
Ryan161020 <ngocquan161020@gmail.com>
2024-11-24 12:05:43 +0700

    add findById catalogy api

--- src/main/java/com/example/demo/controller/CatalogyController.java
+++ src/main/java/com/example/demo/controller/CatalogyController.java
@@ -5,20 +5,22 @@ import com.example.demo.service.CatalogyService;
 import com.example.demo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CatalogyController {
     @Autowired
     private CatalogyService catalogyService;
-    @Autowired
-    private ProductService productService;
+
 
     @PostMapping("/catalogy")
     public ResponseEntity<Object> saveCatalogy(@RequestBody CatalogyEntity catalogy) {
         catalogyService.saveCatalogy(catalogy);
         return ResponseEntity.ok("Catalogy Saved");
+    }
+    @PostMapping(path = "/catalogy/{id}")
+    public ResponseEntity<Object> findByIdCatalogy( @PathVariable("id") Integer id) {
+        catalogyService.findByIdCatalogy(id);
+        return ResponseEntity.ok(id);
     }
 }
